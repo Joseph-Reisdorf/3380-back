77d336bc37b52798e790c8ba4044a5e7e6620a16
BartYu <bartonyu01@gmail.com>
2024-03-31 03:11:14 -0500

    Added Song Upload
    Took way longer than needed

--- routes/album_route.mjs
+++ routes/album_route.mjs
@@ -1,11 +1,13 @@
 import express from "express";
 import { getAlbumById, getAlbumByArtist, getAlbums } from "../controllers/getAlbum.mjs";
-
+import { addAlbum, uploadTracks } from "../controllers/addAlbum.mjs";
 
 const router = express.Router();
 
 router.get("/find_album_by_id/:album_id", getAlbumById);
 router.get("/find_album_by_artist/:artist_id", getAlbumByArtist);
 router.get("/get_albums", getAlbums);
+
+router.post("/add_album", uploadTracks, addAlbum);
 
 export default router;
