80c1c13d74a08e06b133da2f879b490c9204442a
notbali <TayabaliMur@outlook.com>
2024-04-07 04:32:26 -0500

    dashboard metrics files

--- controllers/getDashboardMetrics.mjs
+++ controllers/getDashboardMetrics.mjs
@@ -2,13 +2,13 @@ import db from "../database.mjs";
 
 export const getAgeData = (req, res) => {
     const artist_id = req.params.artist_id;
-    const q =  `SELECT COUNT( *), FLOOR(DATEDIFF(CURRENT_DATE(), person_birthdate) / 365.25) AS listener_age
+    const q =  `SELECT COUNT( *) as count, FLOOR(DATEDIFF(CURRENT_DATE(), person_birthdate) / 365.25) AS listener_age
     FROM artist, follow, listener, person
     WHERE 
         artist.artist_id = follow.artist_id
         AND listener.listener_id = follow.listener_id
         AND listener.listener_id = person.person_id
-        AND artist.artist_id = 8
+        AND artist.artist_id = ?
         group by listener_age `;
   
     db.query(q, [artist_id], (err, data) => {
--- routes/dashboard_route.mjs
+++ routes/dashboard_route.mjs
@@ -3,6 +3,6 @@ import { getAgeData } from "../controllers/getDashboardMetrics.mjs";
 
 const router = express.Router();
 
-router.get("/get_age_data", getAgeData);
+router.get("/get_age_data/:artist_id", getAgeData);
 
 export default router;
