7c60b88422f3f41b7109683dc504f4e3bf0dc1a1
Joseph R <jkreisdo@cougarnet.uh.edu>
2024-04-13 21:28:42 -0500

    Added liked functions, like, unlike, and get liked along with the routes it needed.

--- controllers/getTracks.mjs
+++ controllers/getTracks.mjs
@@ -24,7 +24,9 @@ export const getTrackBlob = (req, res) => {
 
 // Function to get all tracks
 export const getTracks = (req, res) => {
-    const q = "SELECT track_id, track_primary_artist_id, track_name FROM track";
+    const q = `SELECT track_id, track_release_date, track_name, artist_display_name 
+               FROM track, artist
+               WHERE track_primary_artist_id = artist_id`;
     
     db.query(q, (err, tracks) => {
         if (err) return res.status(500).json(err);
@@ -34,6 +36,25 @@ export const getTracks = (req, res) => {
         return res.json(tracks);
     });
 };
+
+export const getLikedTracks = (req, res) => {
+    const listener_id = req.params.listener_id;
+    const q = `
+      SELECT track_id, track_name, track_genre, track_release_date
+      FROM track, track_like
+      WHERE track.track_id = track_like.track_like_track_id
+      AND track_like_listener_id = ?
+    `;
+
+    db.query(q, [listener_id], (err, tracks) => {
+        if (err) return res.status(500).json(err);
+        if (tracks.length === 0) {
+            return res.status(404).json({ message: "No liked tracks found" });
+        };
+        return res.json(tracks);
+    });
+};
+
 
 export const getTracksByAlbumId = (req, res) => {
     const album_id = req.params.album_id;
--- routes/track_route.mjs
+++ routes/track_route.mjs
@@ -3,14 +3,19 @@ import multer from "multer";
 
 import { getTracks, getTracksByAlbumId } from "../controllers/getTracks.mjs";
 import { addTrack } from "../controllers/addTrack.mjs";
+import { trackLike, trackUnlike } from "../controllers/trackLike.mjs";
+import { getLikedTracks } from "../controllers/getTracks.mjs";
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
 router.get("/", getTracks);
 router.get("/get_tracks_by_album/:album_id", getTracksByAlbumId);
-
+router.get("/liked/:listener_id", getLikedTracks);
 
 router.post("/add_track", upload.single('track_mp3'), addTrack);
+router.post("/like/:trackId/:listener_id", trackLike);
+
+router.delete("/unlike/:trackId/:listener_id", trackUnlike);
 
 export default router;
