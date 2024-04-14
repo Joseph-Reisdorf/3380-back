39685ad50774ffa5eaf34e42fbf7ff16b4404e20
Joseph R <jkreisdo@cougarnet.uh.edu>
2024-04-14 01:14:23 -0500

    Added the player and play functions.

--- routes/track_route.mjs
+++ routes/track_route.mjs
@@ -1,7 +1,7 @@
 import express from "express";
 import multer from "multer";
 
-import { getTracks, getTracksByAlbumId } from "../controllers/getTracks.mjs";
+import { getTracks, getTracksByAlbumId, getTrackBlob } from "../controllers/getTracks.mjs";
 import { addTrack } from "../controllers/addTrack.mjs";
 import { trackLike, trackUnlike } from "../controllers/trackLike.mjs";
 import { getLikedTracks } from "../controllers/getTracks.mjs";
@@ -12,6 +12,7 @@ const upload = multer({ storage: multer.memoryStorage() });
 router.get("/", getTracks);
 router.get("/get_tracks_by_album/:album_id", getTracksByAlbumId);
 router.get("/liked/:listener_id", getLikedTracks);
+router.get("/blob/:trackId", getTrackBlob);
 
 router.post("/add_track", upload.single('track_mp3'), addTrack);
 router.post("/like/:trackId/:listener_id", trackLike);
