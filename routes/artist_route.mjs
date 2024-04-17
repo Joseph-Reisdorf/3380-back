1e3451a3ee6d6a61d7d57a5b2205c1197cee465f
heyilly <artemis.lapucelle@gmail.com>
2024-04-17 07:39:55 -0500

    updated artist routing
    it now includes the routing to the diff artist rankings

--- routes/artist_route.mjs
+++ routes/artist_route.mjs
@@ -1,5 +1,5 @@
 import express from "express";
-import { getArtistById, getArtists, searchArtistByName }from "../controllers/getArtist.mjs";
+import { getArtistById, getArtists, searchArtistByName, getArtistRankingByTracks, getArtistRankingByAlbums, getArtistRankingByListens } from "../controllers/getArtist.mjs";
 
 const router = express.Router();
 
@@ -7,5 +7,8 @@ const router = express.Router();
 router.get("/find_artist_by_id/:artist_id", getArtistById); 
 router.get("/search", searchArtistByName);
 router.get("/get_artists", getArtists);
+router.get("/get_artist_ranking_by_tracks", getArtistRankingByTracks);
+router.get("/get_artist_ranking_by_albums", getArtistRankingByAlbums);
+router.get("/get_artist_ranking_by_listens", getArtistRankingByListens);
 
 export default router;
