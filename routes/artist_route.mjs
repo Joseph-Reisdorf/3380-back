b181f8109c2d4fa490d758780696715237d091c7
Joseph R <jkreisdo@cougarnet.uh.edu>
2024-04-18 16:43:48 -0500

    last

--- routes/artist_route.mjs
+++ routes/artist_route.mjs
@@ -1,14 +1,25 @@
 import express from "express";
-import { getArtistById, getArtists, searchArtistByName, getArtistRankingByTracks, getArtistRankingByAlbums, getArtistRankingByListens } from "../controllers/getArtist.mjs";
-
+import { getArtistById, getArtists, searchArtistByName, getArtistRankingByAlbums, getArtistRankingByTracks, getArtistRankingByListens }from "../controllers/getArtist.mjs";
+import { likeArtist, unlikeArtist, getLikedArtists, getArtistLikesCount } from "../controllers/artistLike.mjs";
 const router = express.Router();
 
 // path: /aritists/
 router.get("/find_artist_by_id/:artist_id", getArtistById); 
 router.get("/search", searchArtistByName);
 router.get("/get_artists", getArtists);
+router.get("/get_liked_artists/:person_id", getLikedArtists);
+router.get("/get_artist_likes_count/:artist_id", getArtistLikesCount);
+
+
 router.get("/get_artist_ranking_by_tracks", getArtistRankingByTracks);
 router.get("/get_artist_ranking_by_albums", getArtistRankingByAlbums);
 router.get("/get_artist_ranking_by_listens", getArtistRankingByListens);
+
+
+router.post("/like/:artist_id/:person_id", likeArtist);
+
+router.delete("/unlike/:artist_id/:person_id", unlikeArtist);
+
+
 
 export default router;
